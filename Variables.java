dfd5490bbaaeec2e7b12638ab6b3a19202b5879c
Mohammad Anas <mansurianas1802@gmail.com>
2026-05-09 19:40:24 +0530

    commited

---
+++ Variables.java
@@ -0,0 +1,33 @@
+public class Variables {
+    public static void main (String[] args) {
+    // use all kind of the variables in java for better understing 
+    // integers -- byte,shorts,int,long    
+    byte b = 5;
+    short s = 6;
+    int i = 400;
+    long l = 10000;
+
+    System.out.println("integers values--" + b + "," + s + "," + i + "," + l );
+
+    // floating values 
+    float f = 10.98f;
+    double d = 10.9878;
+
+    System.out.println("float values--"+ f +"," + d);
+
+    // character value 
+    char c = 'a';
+
+    System.out.println("character value--" +c);
+
+    // binary,octal,hexadecimal converstion
+
+    int x = 0b101;
+    int y = 011;
+    int z = 0XA;
+
+    System.out.println("converstion value--"+ x + ","+ y + "," + z);
+
+    }
+    
+}
