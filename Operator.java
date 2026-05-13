5872d84ed7b05bf53a5cd26f397515ec3b01bd63
Mohammad Anas <mansurianas1802@gmail.com>
2026-05-13 17:42:57 +0530

    commited

---
+++ Operator.java
@@ -0,0 +1,20 @@
+public class Operator {
+    public static void main (String[] args)
+    {
+
+    // operator in java
+    // arithmetic operator
+
+    int a = 10 ;
+    int b = 5 ;
+
+    int d = a + b;
+    int e = a - b;
+    int f = a * b;
+    int g = a / b;
+
+    
+    System.out.println("values--" +d+","+e+","+f+","+g);
+
+    }
+}
