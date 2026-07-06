0e9d30c36ba9f3817a1d9b7c5323643789910a2e
Mohammad Anas <mansurianas1802@gmail.com>
2026-07-06 23:14:15 +0530

    commited

---
+++ conditional.java
@@ -0,0 +1,57 @@
+public class conditional {
+    public static void main(String[] args) {
+        /*  boolean b = true ;
+            int i = 5;
+
+            //selection statments
+            //normal if 
+            if (i==5) {
+                System.out.println("i is 5");
+            }
+            else {
+            System.out.println("i is les than 5");
+            }
+                // for the sinlge statment we can ignore the middle brackets
+            
+            */
+            //Nested if
+        /*  if (i > 5) {
+                if(i < 10){
+                if(7 == i){
+
+                }
+                
+                }
+                else {
+
+                }
+            
+            }    
+            else{
+
+            }
+            
+        } 
+    }
+}*/
+
+        // if-else-if ladder 
+            int age = 50;
+        
+            if (age > 80) {
+                System.out.println("you are very old");
+            }
+            else if (age > 60) {
+                System.out.println("you are old");
+            }
+            else if (age > 40) {
+                System.out.println("you are becoming old");
+            }
+            else if (age > 20) {
+                System.out.println("you are young");
+            }
+            else {
+                System.out.println("you are a child");
+            }
+        }        
+}   
