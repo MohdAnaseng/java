23cff4def8108fc472824da398e93648bb005705
Mohammad Anas <mansurianas1802@gmail.com>
2026-07-09 13:45:30 +0530

    commited

--- conditional.java
+++ conditional.java
@@ -1,26 +1,26 @@
 public class conditional {
     public static void main(String[] args) {
-        /*  boolean b = true ;
-            int i = 5;
+            int c = 5;
 
             //selection statments
             //normal if 
-            if (i==5) {
-                System.out.println("i is 5");
+            if (c == 5) {
+                System.out.println("c is 5");
             }
             else {
-            System.out.println("i is les than 5");
+            System.out.println("c is les than 5");
             }
-                // for the sinlge statment we can ignore the middle brackets
+            // for the sinlge statment we can ignore the middle brackets
             
-            */
+            
             //Nested if
-        /*  if (i > 5) {
+            /*int i = 7;
+            if (i > 5) {
                 if(i < 10){
                 if(7 == i){
 
                 }
-                
+
                 }
                 else {
 
@@ -31,11 +31,10 @@ public class conditional {
 
             }
             
-        } 
-    }
-}*/
+        }*/
 
-        // if-else-if ladder 
+
+        // if-else-if ladder conditional  
             int age = 50;
         
             if (age > 80) {
@@ -53,5 +52,24 @@ public class conditional {
             else {
                 System.out.println("you are a child");
             }
+
+
+            //Switch condition
+            int m = 3;
+
+            switch(m) {
+                case 1:
+                    System.out.println("m is 1");
+                    break;
+                case 2:
+                    System.out.println("m is 2");
+                    break;
+                case 3:
+                    System.out.println("m i s 3");
+                    break;
+                default:
+                    System.out.println("m is greater than 3")
+
+            } 
         }        
 }   
